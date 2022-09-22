f211b82d52141734025fce083cbadc566a20d15e
chase-stokes <chasehstokes@gmail.com>
2022-09-22 01:11:02 -0700

    add css modules / update homepage / update layout component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,24 +1,27 @@
 import Link from "next/link"
 import Head from "next/head"
-import Script from "next/script"
+// import Script from "next/script" //used for third party scripts
+import Layout from "../../components/layout"
 
 export default function FirstPost() {
   return (
-    <>
-    <Head>
-      <title>Post #1</title>
-    </Head>
-    <Script
-    src="https://connect.facebook.net/en_US/sdk.js"
-    strategy="lazyOnload"
-    onLoad={() =>
-    console.log(`script loaded correctly, window FB has been populated`)
-    } />
+    <Layout>
+      <Head>
+        <title>Post #1</title>
+      </Head>
+      
+      {/* <Script  
+      src="https://connect.facebook.net/en_US/sdk.js"
+      strategy="lazyOnload"
+      onLoad={() =>
+      console.log(`script loaded correctly, window FB has been populated`)
+      } /> */}
+
       <h1>First Post!!!</h1>
       <h2>
         <Link href='/'>Back to home</Link>
       </h2>
-    </>
+    </Layout>
   )
   
 }
