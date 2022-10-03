c375570f60d48d3784ff1d6c492f0fa7286d4004
chase-stokes <chasehstokes@gmail.com>
2022-10-03 16:33:52 -0700

    fix typos and dates

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({allPostsData}) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi I'm Chase! I am a Fullstack Engineer with a passion for TypeScript, Web Design, and Problem Solving. I am looking forward to the next step in my transition from the service industry into tech. Most recently I have been working as an intern for a Software company developing an app for High School students. Workign on complex features like a Rich Text Editor, Data Visualization, and custon React components has been an increduble and irreplaceable opportunity. I am certain that my experience as a cook and server, which are enviornments that only function correctly when working together, will make me a valuable asset to my future team as an engineer.</p>
+        <p>Hi I'm Chase! I am a Fullstack Engineer with a passion for TypeScript, Web Design, and Problem Solving. I am looking forward to the next step in my transition from the service industry into tech. Most recently I have been working as an intern for a Software company developing an app for High School students. Working on complex features like a Rich Text Editor, Data Visualization, and custon React components has been an increduble and irreplaceable opportunity. I am certain that my experience as a cook and server, which are enviornments that only function correctly when working together, will make me a valuable asset to my future team as an engineer.</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
