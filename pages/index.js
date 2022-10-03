c3210983110f64ba9a4135369291ab2d3b6e31b1
chase-stokes <chasehstokes@gmail.com>
2022-10-03 16:31:25 -0700

    remove link from posts/index

--- pages/index.js
+++ pages/index.js
@@ -22,10 +22,6 @@ export default function Home({allPostsData}) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Hi I'm Chase! I am a Fullstack Engineer with a passion for TypeScript, Web Design, and Problem Solving. I am looking forward to the next step in my transition from the service industry into tech. Most recently I have been working as an intern for a Software company developing an app for High School students. Workign on complex features like a Rich Text Editor, Data Visualization, and custon React components has been an increduble and irreplaceable opportunity. I am certain that my experience as a cook and server, which are enviornments that only function correctly when working together, will make me a valuable asset to my future team as an engineer.</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
